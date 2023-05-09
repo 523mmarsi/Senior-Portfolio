cf96f878949e14b94b4f68145da1a4f06ee797d3
523mmarsi <80544988+523mmarsi@users.noreply.github.com>
2023-05-09 13:22:30 -0400

    Rough Draft #2 05/09/2023

---
+++ script.js
@@ -0,0 +1,38 @@
+//form fields//
+function clearName() {
+    if (document.getElementById("nameField").value == "First and Last Name") {
+      document.getElementById("nameField").value = "";
+    }
+  }
+  function clearPhone() {
+    if (document.getElementById("phoneField").value == "example: 212-123-1234") {
+      document.getElementById("phoneField").value = "";
+    }
+  }
+  function clearEmail() {
+    if (
+      document.getElementById("emailField").value ==
+      "example: blahblahblah@gmail.com"
+    ) {
+      document.getElementById("emailField").value = "";
+    }
+  }
+  function resetName() {
+    if (document.getElementById("nameField").value == "") {
+      document.getElementById("nameField").value = "First and Last Name";
+    }
+  }
+  function resetPhone() {
+    if (document.getElementById("nameField").value == "") {
+      document.getElementById("nameField").value = "example: 212-123-1234";
+    }
+  }
+  function resetEmail() {
+    if (document.getElementById("nameField").value == "") {
+      document.getElementById("nameField").value =
+        "example: blahblahblah@gmail.com";
+    }
+  }
+
+
+
